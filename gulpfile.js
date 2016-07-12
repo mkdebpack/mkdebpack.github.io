893ab971f1bbb28e2b94bcb809484225aeef33bb
mkdebpack <mkdebpack@gmail.com>
2016-07-12 13:59:40 +0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -94,4 +94,4 @@ gulp.task('watch', function () {
  * Default task, running just `gulp` will compile the stylus,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['js', 'stylus', 'jekyll-build']);
+gulp.task('default', ['js', 'stylus', 'imagemin']);
